a242007e0a56f63038532b2a6311acfa2c24e83f
emadvaid <36285415+emadvaid@users.noreply.github.com>
2018-10-29 20:00:19 -0400

    stuff with version display

--- Frontend/AngularApp/src/app/views/file-functions/file-functions.component.ts
+++ Frontend/AngularApp/src/app/views/file-functions/file-functions.component.ts
@@ -27,6 +27,7 @@ export class FileFunctionsComponent implements OnInit {
   public versionNumber =  '1.0.0.1';
   public versionSavedMsg = '';
   public versionSaved = false;
+  model: any = { };
 
 
 
@@ -36,6 +37,7 @@ export class FileFunctionsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.refresh();
   }
 
   dropped(event: UploadEvent) {
@@ -122,6 +124,18 @@ export class FileFunctionsComponent implements OnInit {
       );
   }
 
+  private refresh(): void {
+    this.versionService.getAll().subscribe(
+        (versions: Array<Version>) => {
+          console.log('versions', versions);
+            this.model.versions = versions;
+        },
+        (err: any) => {
+            console.log('VersionComponent: error getting users', err);
+        }
+    );
+}
+
 
   onUpload() {
     console.log('File');
@@ -151,5 +165,8 @@ export class FileFunctionsComponent implements OnInit {
     document.getElementById('button1').setAttribute('disabled', 'true');
     document.getElementById('button2').setAttribute('disabled', 'true');
   }
+  get diagnostics() {
+    return 'model = ' + JSON.stringify(this.model);
+}
 }
 
